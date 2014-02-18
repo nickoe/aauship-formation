9b69b7da761330b437ed82bdeee17eb419b7db9b
Nick Østergaard <oe.nick@gmail.com>
2014-02-18 11:10:39 +0100

    Sinus tingeling for jeppe

--- code/matlab/main.m
+++ code/matlab/main.m
@@ -7,7 +7,7 @@ h1 = figure(1); clf;
 % N = length(Xpos);
 N = 1000;
 Nskip = 20;
-
+heading=sin(1:0.01:N/100);
 
 set(gca,'nextplot','replacechildren');
 set(gcf,'Renderer','zbuffer');
@@ -28,14 +28,14 @@ xdot = zeros(7,N);
 U = zeros(1,N);
 % Main loop
 for i = 1:Nskip:N
-    [xdot(:,i), U(i)] = mariner(x(:,i), -0.1, 5);
+    [xdot(:,i), U(i)] = mariner(x(:,i), heading(i), 5);
     x(:,i+1)=xdot(:,i)+x(:,i);
 %     ship(Xpos(i),Ypos(i),heading(i),'r');
 
     figure(1)
     axis equal
-    xlim([x(4,i)-50 x(4,i)+50])
-    ylim([x(5,i)-50 x(5,i)+50])
+%     xlim([x(4,i)-50 x(4,i)+50])
+%     ylim([x(5,i)-50 x(5,i)+50])
     
     ship(x(4,i),x(5,i),rad2pipi(x(6,i)),'r');
 %     figure(2)
