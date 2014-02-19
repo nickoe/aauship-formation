1e4ae8adde447f0687300b445d701af180cb2894
Nick Østergaard <oe.nick@gmail.com>
2014-02-19 16:31:38 +0100

    Not working path follower

--- code/matlab/main.m
+++ code/matlab/main.m
@@ -1,31 +1,14 @@
-clear all
-h1 = figure(1); clf;
+clear all; clf;
 
-% Xpos = 1:0.1:10;
-% Ypos = 1:0.1:10;
-% heading=sin(Xpos/30);
-% N = length(Xpos);
+Nskip=1;
 N = 3990;
-Nskip = 20;
-%heading=sin(1:0.01:N/100);
-heading = ones(1,N)*pi/2+pi/4;
-
-set(gca,'nextplot','replacechildren');
-set(gcf,'Renderer','zbuffer');
-
-
-mode = 'plot';
-mode = 'anim';
-if strcmp(mode,'anim')
-    Nskip = 1; % Disable skips in animation
-end
-    hold on
-%     plot(Xpos,Ypos,'-')
-    hold off
+% headingdesired=sin(1:0.01:N/100);
+headingdesired = ones(1,N)*pi/2+pi/4;
 
 %x = [ u v r x y psi delta ]'
 x = zeros(7,N);
 xdot = zeros(7,N);
+x(6,1) = (pi/4)*0;
 U = zeros(1,N);
 error = zeros(1,N);
 integral = zeros(1,N);
@@ -33,35 +16,29 @@ derivative = zeros(1,N);
 Kp = 0.4;
 Ki = 10;
 Kd = 0;
-output = zeros(1,N);
+rudderangle = zeros(1,N);
+
+start = [0, 0];
+stop = [1000, 1000];
 
 % Main loop
 for i = 1:Nskip:N
-    [xdot(:,i), U(i)] = mariner(x(:,i), output(i), 5);
-    x(:,i+1)=xdot(:,i)+x(:,i);
-%     ship(Xpos(i),Ypos(i),heading(i),'r');
-    error(i) = heading(i) - x(6,i);
+
+    [xdot(:,i), U(i)] = mariner(x(:,i), rudderangle(i), 5); % SHIP
+    
+    headingdesired(i) = wp_gen(start,stop,x(4:5,i)',U(i)); % WP Gen
+
+    x(:,i+1)=xdot(:,i)+x(:,i); % Integration to positions
+    
+    error(i) = headingdesired(i) - x(6,i);
     integral(i) = integral(i) + error(i);
     if i~=1
     derivative(i) = error(i) - error(i-1);
     end
-    output(i+1) = Kp*error(i) + Ki*integral(i) + Kd*derivative(i);
-        
-%     figure(1)
-%     axis equal
-%     xlim([x(4,i)-50 x(4,i)+50])
-%     ylim([x(5,i)-50 x(5,i)+50])
-    
-%     ship(x(4,i),x(5,i),rad2pipi(x(6,i)),'r');
-
+    rudderangle(i+1) = Kp*error(i) + Ki*integral(i) + Kd*derivative(i);
 end
-    figure(2)
-    subplot(2,1,1)
-    plot(error)
-    subplot(2,1,2)
-    plot(output)
 
-
-
-
-
+figure(1)
+% ship(x(4,:),x(5,:),rad2pipi(x(6,:)),'r');
+plot(x(5,:),x(4,:),'r.',[start(2),stop(2)]',[start(1),stop(1)]','b-o');
+axis equal
