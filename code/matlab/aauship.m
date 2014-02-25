2889c0bec94eaf8720c52312b69e4bc83315bf21
Nick Østergaard <oe.nick@gmail.com>
2014-02-25 12:04:26 +0100

    Fix aauship.m

--- code/matlab/aauship.m
+++ code/matlab/aauship.m
@@ -12,7 +12,7 @@ function [ xdot ] = aauship( x, tau )
 %
 %   See Property 3.1, chap 7.5+
 %   
-%   State vector: x = [u v r x y theta]
+%   State vector: x = [u v r x y psi]
 
 % Check of input and state dimensions
 if (length(x)  ~= 6),error('x-vector must have dimension 6!');end
@@ -24,15 +24,29 @@ m = 12; %[kg]
 % Friction coefficients
 kx = -0.01;
 ky = -0.1;
-kz = -0.1;
+kz = -0;
+% kx = -0;
+% ky = -0;
+% kz = -0;
 
-% WARNING thist is only including the one propeller!!!! we need to remmeber
-% the other else we will just be circling or something like that
+
+
 xdot(1) = kx*abs(x(1))*x(1) + ( (cos(theta)^2)*tau(1) + (cos(theta)^2)*tau(2) )/m;
 xdot(2) = ky*abs(x(2))*x(2) + ( cos(theta)*sin(theta)*tau(1) - cos(theta)*sin(theta)*tau(2) )/m;
+
 xdot(3) = kz*abs(x(3))*x(3) + ( sin(theta)*tau(1) - sin(theta)*tau(2) )/m;
-xdot(4) = xdot(1) + x(1);
-xdot(5) = xdot(2) + x(2);
-xdot(6) = xdot(3) + x(3);
+
+xdot(6) = rad2pipi(xdot(3)); %
+
+% psi = xdot(6);
+psi = x(3);
+% xdot(6) = xdot(6);
+
+R = [cos(psi) -sin(psi);...
+     sin(psi) cos(psi)];
+
+xdot(4:5) = R*x(1:2) ;
+
+
 end
 
--- code/matlab/aaushiptest.m
+++ code/matlab/aaushiptest.m
@@ -1,6 +1,7 @@
 % test for aauship.m
 clear all
-N = 30;
+N = 40;
+clf
 
 
 %x = [ u v r x y psi ]'
@@ -8,16 +9,17 @@ x = zeros(6,N);
 xdot = zeros(6,N);
 
 for i = 1:N
-    [xdot(1:6,i)] = aauship(x(1:6,i), [70;0]); % SHIP
+    [xdot(1:6,i)] = aauship(x(1:6,i), [5;10]); % SHIP
     x(:,i+1)=xdot(:,i)+x(:,i); % Euler integration, now assuming dt = 1, dt*xdot when not true
 end
 
 subplot(2,1,1)
-plot(x(5,:),x(4,:),'r.');
+plot(x(5,:),x(4,:),'-r.');
+ship(x(5,1:N),x(4,1:N),rad2pipi(-1*x(6,1:N)+pi/2),'r');
 ylabel('Northing [m]')
 xlabel('Easting [m]')
+axis equal
 subplot(2,1,2)
-plot(xdot(6,:))
+plot(x(6,:),'.-')
 xlabel('Time [samples]')
 ylabel('Heading angle [rad]')
-axis equal
