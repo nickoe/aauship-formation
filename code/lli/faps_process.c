d2c0a00cb833cf45ad7c678fb8e1b607c2cdab69
Nick Østergaard <oe.nick@gmail.com>
2014-04-11 13:47:16 +0200

    FIX VERY ANNOYING BUG

--- code/lli/faps_process.c
+++ code/lli/faps_process.c
@@ -6,6 +6,7 @@
 #include "crc16.h"
 #include <string.h>
 
+
 int awake_flag;
 /*
  * Decide what to do with a given command
@@ -134,9 +135,6 @@ int process(msg_t *msg)
 				case 20:
 					break;
 				case 21:
-					//duty = (int16_t) (msg->data[0]);
-					//duty = (duty << 8) & 0xFF00;
-					//duty = (duty | ((msg->data[1])&0xFF));
 					duty = (int16_t) ((msg->data[0] << 8) & 0xFF00) | (msg->data[1]&0xFF);
               if (duty == 0)
                 PORTF &= ~(1<<DCDIR1);
@@ -146,9 +144,6 @@ int process(msg_t *msg)
 					awake_flag = 0;
 					break;
 				case 22:
-					//duty = (int16_t) (msg->data[0]);
-					//duty = (duty << 8) & 0xFF00;
-					//duty = (duty | ((msg->data[1])&0xFF));
 					duty = (int16_t) ((msg->data[0] << 8) & 0xFF00) | (msg->data[1]&0xFF);
               if (duty == 0)
                 PORTF &= ~(1<<DCDIR2);
@@ -158,9 +153,6 @@ int process(msg_t *msg)
 					awake_flag = 0;
 					break;
 				case 23:
-					//duty = (int16_t) (msg->data[0]);
-					//duty = (duty << 8) & 0xFF00;
-					//duty = (duty | ((msg->data[1])&0xFF));
 					duty = (int16_t) ((msg->data[0] << 8) & 0xFF00) | (msg->data[1]&0xFF);
               if (duty == 0)
                 PORTF &= ~(1<<DCDIR3);
@@ -180,29 +172,6 @@ int process(msg_t *msg)
 					break;
 
 			}
-
-		// This must be some old debugging id's
-		case 101: 
-			pwm_set_duty(DC2, msg->data[0]);
-			break;
-		case 102: 
-			pwm_set_duty(DC3, msg->data[0]);
-			break;
-		case 104: 
-			pwm_set_duty(RC2, msg->data[0]);
-			break;
-		case 105: 
-			pwm_set_duty(RC3, msg->data[0]);
-			break;
-		case 106: 
-			pwm_set_duty(RC4, msg->data[0]);
-			break;
-		case 107: 
-			pwm_set_duty(RC5, msg->data[0]);
-			break;
-		case 108: 
-			PORTL ^= (1<<LED4);
-			break;
 	}
 }
 
