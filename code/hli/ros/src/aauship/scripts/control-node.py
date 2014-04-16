8ed7a743aa8c828e1e34b9d8e2cc0a33f7662961
Nick Østergaard <oe.nick@gmail.com>
2014-04-16 10:26:12 +0200

    Much control, very debuggy, so brilliant, wow.

--- code/hli/ros/src/aauship/scripts/control-node.py
+++ code/hli/ros/src/aauship/scripts/control-node.py
@@ -16,24 +16,26 @@ class Control(object):
 #        rospy.loginfo(rospy.get_caller_id()+"I heard %s",data.data)
 #        self.ctllog.write(data.data)
         # Publish data to lli_input
-        print "control callback" + time.time()
+        print "control callback" + str(time.time())
+        print os.getcwd()
+        print os.environ
         pass
 
     def run(self):
 	BUFSIZE = 1024
         #self.ctllog = open(os.getcwd() + "/../meas/ctl.log",'w')
-        self.ctllog = open("logs/ctl.log",'w',BUFSIZE)
-        print(self.ctllog.name)
+        ##self.ctllog = open("logs/ctl.log",'w',BUFSIZE)
+        ##print(self.ctllog.name)
         sub = rospy.Subscriber('control_input', Faps, self.callback)
         pub = rospy.Publisher('lli_input', Faps)
         rospy.init_node('control')
         r = rospy.Rate(0.5) # Hz
         #rospy.spin() # Keeps the node running untill stopped
         while not rospy.is_shutdown():
-            pub.publish("control signals should be sent here")
+            #pub.publish("control signals should be sent here")
             r.sleep()
         print("\nClosing log file")
-        self.ctllog.close()
+        ##self.ctllog.close()
         print("Exiting")
         exit()
 
