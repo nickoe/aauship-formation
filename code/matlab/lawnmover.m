8b7d0b237e904fa600beb741690aa2ca3a1879b8
Jeppe Dam <jdam09@student.aau.dk>
2014-05-28 13:35:36 +0200

    så er der rundinger på lawnmower

--- code/matlab/lawnmover.m
+++ code/matlab/lawnmover.m
@@ -5,8 +5,8 @@
 %%
 clear all
 % Box options
-boxheigth = 500;
-boxwidth = 700;
+boxheigth = 600;
+boxwidth = 600;
 
 ll = [0,0];
 lr = [boxwidth,0];
@@ -15,37 +15,48 @@ ul = [0,boxheigth];
 
 
 % Turning restrictions
-tr = 40; % turning radius
+tr = 10; % turning radius
+s = 2*tr;
 n = 1;
 figure(1)
 clf
 hold on
-while ((lr(2) + n*tr) <= (ur(2) - tr))
-rightwps(n,:) = [lr(1)-tr , lr(2)+n*tr]
-leftwps(n,:) = [tr,rightwps(n,2)]
+while ((lr(2) + n*s) <= (ur(2) - s))
+rightwps(n,:) = [lr(1)-s , lr(2)+n*s]
+leftwps(n,:) = [s,rightwps(n,2)]
 plot([leftwps(n,1),rightwps(n,1)],[leftwps(n,2),rightwps(n,2)])
 n = n+1
 end
+
+y = [-tr:1:tr]';
+x = sqrt((tr)^2-y.^2+tr/2);
+cl = length(x)-1;
+
 N=n-1;
 k=1
 for n = 1:N
 if mod(n,2)==1 %ulige
-    allwps(k,:) = leftwps(n,:)
+    allwps(k,:) = leftwps(n,:);
     k = k+1
-    allwps(k,:) = rightwps(n,:)
+    allwps(k,:) = rightwps(n,:);
     k = k+1
+    allwps(k:k+cl,:) = [+(x+allwps(k-1,1)),y+tr+allwps(k-1,2)];
+    k = k+cl;
     n = n+1
 elseif mod(n,2)==0 %lige
-    allwps(k,:) = rightwps(n,:)
+    allwps(k,:) = rightwps(n,:);
     k = k+1
-    allwps(k,:) = leftwps(n,:)
+    allwps(k,:) = leftwps(n,:);
     k = k+1
+    allwps(k:k+cl,:) = [-(x-allwps(k-1,1)),y+tr+allwps(k-1,2)];
+    k = k+cl;
     n = n+1
 end
 end
 
-plot(allwps(:,1),allwps(:,2))
-
+save('track.mat','allwps')
+plot(allwps(:,1),allwps(:,2),'-')
+axis equal
 
 
 
