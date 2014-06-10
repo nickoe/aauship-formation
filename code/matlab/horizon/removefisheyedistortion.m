aebc246757385aa4dacf0d1561fb961c71d54b27
Nick Østergaard <oe.nick@gmail.com>
2014-06-10 15:50:07 +0200

    Add script to remoce the fisheye effect of the gopro.

--- code/matlab/horizon/removefisheyedistortion.m
+++ code/matlab/horizon/removefisheyedistortion.m
@@ -1,24 +1,25 @@
 % Create a cell array of file names of calibration images.
-    for i = 1:10
-%         imageFileName = sprintf('image%02d.jpg', i);
-        imageFileName = sprintf('GOPR3938.JPG', i);
-        imageFileNames{i} = fullfile(matlabroot, 'toolbox', 'vision','visiondemos', 'calibration', 'fishEye', imageFileName);
+    for i = 1:6
+        imageFileName = sprintf('%d.JPG', i);
+        imageFileNames{i} = fullfile('img', imageFileName);
     end
 
 % Detect calibration pattern.
     [imagePoints, boardSize] = detectCheckerboardPoints(imageFileNames);
 
 % Generate world coordinates of the corners of the squares.
-    squareSize = 29; % square size in millimeters
+    squareSize = 34; % square size in millimeters
     worldPoints = generateCheckerboardPoints(boardSize, squareSize);
 
 % Calibrate the camera.
     cameraParameters = estimateCameraParameters(imagePoints, worldPoints);
 
 % Remove lens distortion and display results
-    I = imread(fullfile(matlabroot, 'toolbox', 'vision', 'visiondemos', 'calibration', 'fishEye', 'image01.jpg'));
+    I = imread(fullfile('img', '5.JPG'));
     J = undistortImage(I, cameraParameters);
 
 % Display original and corrected image.
     figure; imshowpair(I, J, 'montage');
     title('Original Image (left) vs. Corrected Image (right)');
+    
+%     save('gopro-hero3-cameraParameters.mat','cameraParameters')
