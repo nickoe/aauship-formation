b1223d967b6102e75d82a6662d9a77b83dd7dd62
Nick Østergaard <oe.nick@gmail.com>
2014-07-16 15:54:20 +0200

    Clean up a bit

--- code/hli/ros/src/aauship/src/ahrs-mahony-node.cpp
+++ code/hli/ros/src/aauship/src/ahrs-mahony-node.cpp
@@ -10,7 +10,6 @@
 // in rviz.
 
 // Construct filter
-//AHRS u(8.8, 0.1, 10);
 MahonyAHRS u(8, 0.4, 12);
 MadgwickAHRS p(3,12);
 
@@ -31,26 +30,22 @@ void adisCallback(const aauship::ADIS16405::ConstPtr& msg)
 //0.28 0.15 -0.18
   printf("gyro [% f, % f, % f]\r\n", (msg->xgyro*0.05-1.65)*3.1415/180, (msg->ygyro*0.05+16.15)*3.1415/180, (msg->zgyro*0.05+5.4)*3.1415/180);
   printf("accl [% f, % f, % f]\r\n", (msg->xaccl*0.00333), (msg->yaccl*0.00333), (msg->zaccl*0.00333));
-//  printf("magn [% f, % f, % f]\r\n", msg->xmagn*0.0005-0.28, msg->ymagn*0.0005-0.15, msg->zmagn*0.0005-(-0.18));
   printf("magn [% f, % f, % f]\r\n", msg->xmagn, msg->ymagn, msg->zmagn);
   
   /* AHRS debug output */
 //  ROS_INFO("Quaternions: [%.3f, %.3f, %.3f, %0.3f]", u.getQuaternions(1), u.getQuaternions(2), u.getQuaternions(3), u.getQuaternions(0));
   u.calculateEulerAngles();
-//  ROS_INFO("Euler angles: [%.3f, %.3f, %.3f]", u.getEulerAngles(0), u.getEulerAngles(1), u.getEulerAngles(2));
-//  ROS_INFO("getTuning: [%.3f, %.3f]", u.getTuning(0), u.getTuning(1));
-//  ROS_INFO("sampleFreq: [%.3f]", u.getSampleFreq());
   
-  /* Publish rviz information */
+  /* Publish rviz transform information */
   static tf::TransformBroadcaster tfbc;
   tf::Transform transform;
   transform.setOrigin( tf::Vector3(0,0,0) );
 
+	/* Mahony filter results */
 //  tf::Quaternion q(u.getQuaternions(1),u.getQuaternions(2),u.getQuaternions(3),u.getQuaternions(0));
+  /* Madgwick filter results */
   tf::Quaternion q(p.getQuaternions(1),p.getQuaternions(2),p.getQuaternions(3),p.getQuaternions(0));
 
-//  tf::Quaternion q;
-//  q.setRPY(u.getEulerAngles(0), u.getEulerAngles(1), u.getEulerAngles(2));
   transform.setRotation(q);
   tfbc.sendTransform( tf::StampedTransform(transform, ros::Time::now(), "map", "boat_link"));
 }
@@ -65,7 +60,6 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "ahrs_mahony");
   ros::NodeHandle adis;
-//  ros::Subscriber adissub = adis.subscribe("adis16405", 2, adisCallback);
   ros::Subscriber adissub = adis.subscribe("imu", 1, adisCallback);
   ros::NodeHandle ahrs;
   ros::Subscriber ahrssub = ahrs.subscribe("ahrs", 2, ahrsCallback);
