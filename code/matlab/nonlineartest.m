c16501e031adff3646bcfd4cfa672eb6d1e66e6f
Nick Østergaard <oe.nick@gmail.com>
2014-08-06 12:26:23 +0200

    Add testcase document

---
+++ code/matlab/nonlineartest.m
@@ -0,0 +1,11 @@
+clear all;
+
+load('ssaauship.mat');
+
+% Test wether x = aauship(x,[0 0 0 0 0]) is the same as x = R*Ad*x
+% tic
+% toc
+% Elapsed time is 0.239399 seconds.
+% tic
+% toc
+% Elapsed time is 0.200266 seconds.
