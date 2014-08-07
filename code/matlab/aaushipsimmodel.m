43aaf5c6806a18a2ebdb8ed6abf8ebe9ba09bb63
Nick Østergaard <oe.nick@gmail.com>
2014-08-07 17:48:26 +0200

    Simulatio model, kind of works, but it seems the acceleration is not quite right. It is very small, and does not stabilize to zero.

---
+++ code/matlab/aaushipsimmodel.m
@@ -0,0 +1,35 @@
+function [ xn, eta, nu ] = aaushipsimmodel( x, tau, ts )
+%AAUSHIP Control Model of AAUSHIHP
+%   This is a simple model of AAUSHIP, supposed to be a simulation model
+%   
+%   Since AAUSHIP is a twin propeller ship and has no rudder angle the
+%   interface for this is different than the other vessel models used in
+%   the MSS toolbox.
+%
+%   See Property 3.1, chap 7.5+
+%   
+%   State vector: x = [x y phi theta psi u v p q r]'
+%   Position vector: eta = [N E phi theta psi]'
+%   Velocity vector: nu = [u v p q r]'
+%   Force input vector: tau = [X Y K M N]'
+
+% Check of input and state dimensions
+if (length(x)  ~= 10),error('x-vector must have dimension 10!');end
+if (length(tau) ~= 5),error('tau-vector must have dimension 5!');end
+
+ss = load('ssaauship.mat');
+
+xn = ss.Ad*x + ss.Bd*tau;
+
+xn(5) = xn(10)*ts + x(5);
+psi=xn(5);
+Rz = [cos(psi) -sin(psi);
+      sin(psi)  cos(psi)];
+R = diag(ones(5,1));
+R(1:2,1:2) = Rz(1:2,1:2);
+eta(1:5) = x(1:5) + ts*R*xn(6:10);
+xn(1:5) = eta(1:5);
+
+eta(3:5) = xn(8:10)*ts+x(3:5);
+nu = xn(6:10);
+end
---
+++ code/matlab/simulationmodetest.m
@@ -0,0 +1,58 @@
+% This tests the simulaiton model and plots stuff
+clear all;
+
+
+% Input vector
+tau = [6 0 0 0 -0.1]';
+
+% Sample period
+ts = 0.1;
+
+% Number of samples
+N = 500;
+
+x = zeros(10,N);
+eta = zeros(5,N);
+nu = zeros(5,N);
+
+
+% Initial state
+x(:,1) = [0 0 0 0 0.7854 0 0 0 0 0]';
+
+for k = 1:N-1
+    if k > 200
+        tau = [ 0 0 0 0 0.1 ]';
+    end
+    [x(:,k+1), eta(:,k+1), nu(:,k+1)] = aaushipsimmodel( x(:,k), tau, ts );
+    
+    H = [1 0 0 0 0 0 0 0 0 0;
+         0 1 0 0 0 0 0 0 0 0;
+         0 0 0 0 1 0 0 0 0 0;
+         0 0 0 0 0 1 0 0 0 0;
+         0 0 0 0 0 0 1 0 0 0;
+         0 0 0 0 0 (x(6,k+1)-x(6,k)) 0 0 0 0;
+         0 0 0 0 0 0 (x(7,k+1)-x(7,k)) 0 0 0];
+    z = H*x;
+end
+
+figure(1)
+subplot(2,2,1)
+plot(eta(2,:),eta(1,:),'-')
+axis equal
+xlabel('Easting (m)')
+ylabel('Norting (m)')
+
+subplot(2,2,2);
+plot(ts:ts:N*ts,eta(5,:))
+xlabel('Time (s)')
+ylabel('Heading (rad)')
+
+subplot(2,2,3);
+plot(ts:ts:N*ts,nu(1,:), ts:ts:N*ts,nu(2,:))
+xlabel('Time (s)')
+ylabel('Speed (m/s)')
+
+subplot(2,2,4);
+plot(ts:ts:N*ts,z(6,:), ts:ts:N*ts,z(7,:))
+xlabel('Time (s)')
+ylabel('Acceleration (m/s^2)')
