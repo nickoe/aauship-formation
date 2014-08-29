bc246122a051b540de13ef3458238d4b081aac50
Nick Østergaard <oe.nick@gmail.com>
2014-08-29 11:50:09 +0200

    Som KF crap in cpp that do not work

--- code/hli/ros/src/aauship/src/KalmanFilter.cpp
+++ code/hli/ros/src/aauship/src/KalmanFilter.cpp
@@ -1,46 +1,15 @@
 // // Jeppe forsoeger at imp kalmanFilter
-// #include "ros/ros.h"
-// #include "aauship/ADIS16405.h"
-// #include <math.h>'
-// 
-// KalmanFilter::KalmanFilter(void){
-// 	PHI = [];
-// 
-// 
-// 
-// }
-// 
-// void KalmanFilter::KalmanFilterUpdate(){
-// 
-// 
-// 
-// }
+#include "ros/ros.h"
+#include "aauship/KalmanFilter.h"
 
-/* example-009.cc
-*
-* Copyright (C) 2011-2014 Jeremy Fix
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 3 of the License, or (at
-* your option) any later version.
-*
-* This program is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-* General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-*/
-/* In this example, we use EKF for state/parameter estimation in order to estimate the state and parameters of a Lorentz attractor */
 #include <cstdlib>
 #include <cmath>
 #include <iostream>
 #include <fstream>
+#include <aauship/ekf.h>
 #include <aauship/ukf.h>
 #include <sys/time.h>
+
 using namespace ekf;
 //#define SIGMA 10.0
 //#define RHO 28.0
@@ -70,11 +39,195 @@ using namespace ekf;
 #define NOISE_AMPLITUDE 2.0
 #define VERBOSE true
 
+
+// KF update step
+void KalmanFilter::KalmanFilterUpdate(void) { // should not have void parameeters
+int i=0;
+
+    struct timeval before, after;
+    srand(time(NULL));
+		//  Above should probably be removed
+
+    
+    // Definition of the parameters and state variables
+    ekf_param p;
+    ekf_state s;
+    // The parameters for the evolution equation
+    s.params = gsl_vector_alloc(1);
+    s.params->data[0] = DT;
+    // Initialization of the parameters
+    p.n = 17;
+    p.no = 7;
+    //EvolutionNoise * evolution_noise = new ekf::EvolutionAnneal(1e-2, 0.99, 1e-8);
+    EvolutionNoise * evolution_noise = new ekf::EvolutionRLS(1e-2, 0.9995);
+    //EvolutionNoise * evolution_noise = new ekf::EvolutionRobbinsMonro(1e-5, 1e-6);
+    p.evolution_noise = evolution_noise;
+    p.observation_noise = 1e-1;
+    p.prior_pk = 1.0;
+    p.observation_gradient_is_diagonal = true;
+    // Initialization of the state and parameters
+    ekf_init(p,s);
+ 
+ 
+		// Initialize the parameter vector to some random values
+    for(int i = 0 ; i < p.n ; i++)
+        gsl_vector_set(s.xk,i,5.0*(2.0*rand()/double(RAND_MAX-1)-1.0));
+    s.xk->data[0] = -15.0;
+    s.xk->data[1] = -15.0;
+    s.xk->data[2] = -15.0;
+    // Allocate the input/output vectors
+    gsl_vector * xi = gsl_vector_alloc(p.n);
+    gsl_vector * yi = gsl_vector_alloc(p.no);
+    gsl_vector_set_zero(yi);
+    /***********************************************/
+    /***** Iterate the learning on the samples *****/
+    /***********************************************/
+    int epoch = 0;
+    xi->data[0]  = N0   ;
+    xi->data[1]  = E0   ;
+    xi->data[2]  = X0   ;
+    xi->data[3]  = Y0   ;
+    xi->data[4]  = PHI  ;
+    xi->data[5]  = THETA;
+    xi->data[6]  = PSI  ;
+    xi->data[7]  = U    ;
+    xi->data[8]  = V    ;
+    xi->data[9]  = P    ;
+    xi->data[10] = Q    ;
+    xi->data[11] = R    ;
+    xi->data[12] = DOTU ;
+    xi->data[13] = DOTV ;
+    xi->data[14] = DOTP ;
+    xi->data[15] = DOTQ ;
+    xi->data[16] = DOTR ;
+    std::ofstream outfile("example-009.data");
+    std::ofstream outfile_rms("example-009-rms.data");
+    double rms= 0.0;
+    double errorBound = NOISE_AMPLITUDE/2.0;
+    int count_time=0;
+    int nb_steps_below_error=1000;
+    //int is_counting=0;
+    gettimeofday(&before, NULL);
+    outfile << epoch << " ";
+    for(int i = 0 ; i < 17 ; ++i)
+        outfile << xi->data[i] << " " ;
+    for(int i = 0 ; i < 7 ; ++i)
+        outfile << yi->data[i] << " " ;
+    for(int i = 0 ; i < 17 ; ++i)
+        outfile << s.xk->data[i] << " " ;
+    outfile << std::endl;
+    while( epoch < 8000)
+    {
+    //printf("Epoch %i \n", epoch);
+    // Evaluate the true dynamical system
+    f(s.params, xi, xi);
+    h(s.params, xi, yi);
+    // Provide the observation and iterate
+//    ekf_iterate(p,s,f,df,h,dh,yi);
+    epoch++;
+    rms = 0.0;
+    for(int i = 0 ; i < p.n ; ++i)
+        rms += gsl_pow_2(xi->data[i] - s.xk->data[i]);
+        rms /= double(p.n);
+        rms = sqrt(rms);
+        if(rms <= errorBound)
+        {
+            count_time++;
+            if(count_time >= nb_steps_below_error)
+            break;
+        }
+        else
+        {
+            count_time = 0;
+        }
+        printf("[%i] True state : %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e, estimated : %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e\n", epoch,
+            xi->data[0] ,
+            xi->data[1] ,
+            xi->data[2] ,
+            xi->data[3] ,
+            xi->data[4] ,
+            xi->data[5] ,
+            xi->data[6] ,
+            xi->data[7] ,
+            xi->data[8] ,
+            xi->data[9] ,
+            xi->data[10],
+            xi->data[11],
+            xi->data[12],
+            xi->data[13],
+            xi->data[14],
+            xi->data[15],
+            xi->data[16],
+            s.xk->data[0] ,
+            s.xk->data[1] ,
+            s.xk->data[2] ,
+            s.xk->data[3] ,
+            s.xk->data[4] ,
+            s.xk->data[5] ,
+            s.xk->data[6] ,
+            s.xk->data[7] ,
+            s.xk->data[8] ,
+            s.xk->data[9] ,
+            s.xk->data[10],
+            s.xk->data[11],
+            s.xk->data[12],
+            s.xk->data[13],
+            s.xk->data[14],
+            s.xk->data[15],
+            s.xk->data[16]);
+        outfile << epoch << " ";
+        for(int i = 0 ; i < 17 ; ++i)
+            outfile << xi->data[i] << " " ;
+        for(int i = 0 ; i < 7 ; ++i)
+            outfile << yi->data[i] << " " ;
+        for(int i = 0 ; i < 17 ; ++i)
+            outfile << s.xk->data[i] << " " ;
+        outfile << std::endl;
+        outfile_rms << rms << std::endl;
+    }
+    gettimeofday(&after, NULL);
+    double sbefore = before.tv_sec + before.tv_usec * 1E-6;
+    double safter = after.tv_sec + after.tv_usec * 1E-6;
+    double total = safter - sbefore;
+    outfile.close();
+    outfile_rms.close();
+    std::cout << " Run on " << epoch << " epochs ;" << std::scientific << total / double(epoch) << " s./step " << std::endl;
+    printf("I found the following parameters : %e %e %e ; The true parameters being : %e %e %e \n", s.xk->data[0], s.xk->data[1], s.xk->data[6], N0, E0, PSI);
+    std::cout << " Outputs are saved in example-009*.data " << std::endl;
+    std::cout << " You can plot them using e.g. gnuplot : " << std::endl;
+    //std::cout << " gnuplot Data/plot-example-009.gplot ; gv Output/example-009-rms.ps ; gv Output/example-009-Lorentz.ps " << std::endl;
+    /***********************************************/
+    /**** Free the memory ****/
+    /***********************************************/
+    ekf_free(p,s);
+
+}
+
+/* example-009.cc
+*
+* Copyright (C) 2011-2014 Jeremy Fix
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 3 of the License, or (at
+* your option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+/* In this example, we use EKF for state/parameter estimation in order to estimate the state and parameters of a Lorentz attractor */
+
 /*************************************************************************************/
 /* Definition of the evolution and observation functions */
 /*************************************************************************************/
 // Evolution function
-void f(gsl_vector * params, gsl_vector * xk_1, gsl_vector * xk)
+void KalmanFilter::f(gsl_vector * params, gsl_vector * xk_1, gsl_vector * xk)
 {
     double n = gsl_vector_get(xk_1,0);
     double e = gsl_vector_get(xk_1,1);
@@ -114,7 +267,7 @@ void f(gsl_vector * params, gsl_vector * xk_1, gsl_vector * xk)
 }
 
 // Jacobian of the evolution function
-void df(gsl_vector * params, gsl_vector * xk_1, gsl_matrix * Fxk)
+void KalmanFilter::df(gsl_vector * params, gsl_vector * xk_1, gsl_matrix * Fxk)
 {
     double n = gsl_vector_get(xk_1,0);
     double e = gsl_vector_get(xk_1,1);
@@ -443,178 +596,26 @@ void df(gsl_vector * params, gsl_vector * xk_1, gsl_matrix * Fxk)
 }
 
 // Observation function
-void h(gsl_vector * params, gsl_vector * xk , gsl_vector * yk)
+void KalmanFilter::h(gsl_vector * params, gsl_vector * xk , gsl_vector * yk)
 {
     for(unsigned int i = 0 ; i < yk->size ; ++i)
     gsl_vector_set(yk, i, gsl_vector_get(xk,i) + NOISE_AMPLITUDE*rand()/ double(RAND_MAX));
 }
 
 // Jacobian of the observation function
-void dh(gsl_vector * params, gsl_vector * xk , gsl_matrix * Hyk)
+void KalmanFilter::dh(gsl_vector * params, gsl_vector * xk , gsl_matrix * Hyk)
 {
     gsl_matrix_set_zero(Hyk);
     gsl_matrix_set(Hyk, 0, 0, 1.0);
     gsl_matrix_set(Hyk, 1, 1, 1.0);
-    gsl_matrix_set(Hyk, 2, 2, 1.0);
-    gsl_matrix_set(Hyk, 3, 3, 1.0);
-    gsl_matrix_set(Hyk, 4, 4, 1.0);
-    gsl_matrix_set(Hyk, 5, 5, 1.0);
-    gsl_matrix_set(Hyk, 6, 6, 1.0);
+    gsl_matrix_set(Hyk, 2, 6, 1.0);
+    gsl_matrix_set(Hyk, 3, 7, 1.0);
+    gsl_matrix_set(Hyk, 4, 8, 1.0);
+    gsl_matrix_set(Hyk, 5, 12, 1.0);
+    gsl_matrix_set(Hyk, 6, 13, 1.0);
 }
 
-/*****************************************************/
-/* main */
-/*****************************************************/
-int main(int argc, char* argv[]) {
-    struct timeval before, after;
-    srand(time(NULL));
-    // Definition of the parameters and state variables
-    ekf_param p;
-    ekf_state s;
-    // The parameters for the evolution equation
-    s.params = gsl_vector_alloc(1);
-    s.params->data[0] = DT;
-    // Initialization of the parameters
-    p.n = 17;
-    p.no = 7;
-    //EvolutionNoise * evolution_noise = new ekf::EvolutionAnneal(1e-2, 0.99, 1e-8);
-    EvolutionNoise * evolution_noise = new ekf::EvolutionRLS(1e-2, 0.9995);
-    //EvolutionNoise * evolution_noise = new ekf::EvolutionRobbinsMonro(1e-5, 1e-6);
-    p.evolution_noise = evolution_noise;
-    p.observation_noise = 1e-1;
-    p.prior_pk = 1.0;
-    p.observation_gradient_is_diagonal = true;
-    // Initialization of the state and parameters
-    ekf_init(p,s);
-    // Initialize the parameter vector to some random values
-    for(int i = 0 ; i < p.n ; i++)
-        gsl_vector_set(s.xk,i,5.0*(2.0*rand()/double(RAND_MAX-1)-1.0));
-    s.xk->data[0] = -15.0;
-    s.xk->data[1] = -15.0;
-    s.xk->data[2] = -15.0;
-    // Allocate the input/output vectors
-    gsl_vector * xi = gsl_vector_alloc(p.n);
-    gsl_vector * yi = gsl_vector_alloc(p.no);
-    gsl_vector_set_zero(yi);
-    /***********************************************/
-    /***** Iterate the learning on the samples *****/
-    /***********************************************/
-    int epoch = 0;
-    xi->data[0]  = N0   ;
-    xi->data[1]  = E0   ;
-    xi->data[2]  = X0   ;
-    xi->data[3]  = Y0   ;
-    xi->data[4]  = PHI  ;
-    xi->data[5]  = THETA;
-    xi->data[6]  = PSI  ;
-    xi->data[7]  = U    ;
-    xi->data[8]  = V    ;
-    xi->data[9]  = P    ;
-    xi->data[10] = Q    ;
-    xi->data[11] = R    ;
-    xi->data[12] = DOTU ;
-    xi->data[13] = DOTV ;
-    xi->data[14] = DOTP ;
-    xi->data[15] = DOTQ ;
-    xi->data[16] = DOTR ;
-    std::ofstream outfile("example-009.data");
-    std::ofstream outfile_rms("example-009-rms.data");
-    double rms= 0.0;
-    double errorBound = NOISE_AMPLITUDE/2.0;
-    int count_time=0;
-    int nb_steps_below_error=1000;
-    //int is_counting=0;
-    gettimeofday(&before, NULL);
-    outfile << epoch << " ";
-    for(int i = 0 ; i < 17 ; ++i)
-        outfile << xi->data[i] << " " ;
-    for(int i = 0 ; i < 7 ; ++i)
-        outfile << yi->data[i] << " " ;
-    for(int i = 0 ; i < 17 ; ++i)
-        outfile << s.xk->data[i] << " " ;
-    outfile << std::endl;
-    while( epoch < 8000)
-    {
-    //printf("Epoch %i \n", epoch);
-    // Evaluate the true dynamical system
-    f(s.params, xi, xi);
-    h(s.params, xi, yi);
-    // Provide the observation and iterate
-    ekf_iterate(p,s,f,df,h,dh,yi);
-    epoch++;
-    rms = 0.0;
-    for(int i = 0 ; i < p.n ; ++i)
-        rms += gsl_pow_2(xi->data[i] - s.xk->data[i]);
-        rms /= double(p.n);
-        rms = sqrt(rms);
-        if(rms <= errorBound)
-        {
-            count_time++;
-            if(count_time >= nb_steps_below_error)
-            break;
-        }
-        else
-        {
-            count_time = 0;
-        }
-        printf("[%i] True state : %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e, estimated : %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e %e\n", epoch,
-            xi->data[0] ,
-            xi->data[1] ,
-            xi->data[2] ,
-            xi->data[3] ,
-            xi->data[4] ,
-            xi->data[5] ,
-            xi->data[6] ,
-            xi->data[7] ,
-            xi->data[8] ,
-            xi->data[9] ,
-            xi->data[10],
-            xi->data[11],
-            xi->data[12],
-            xi->data[13],
-            xi->data[14],
-            xi->data[15],
-            xi->data[16],
-            s.xk->data[0] ,
-            s.xk->data[1] ,
-            s.xk->data[2] ,
-            s.xk->data[3] ,
-            s.xk->data[4] ,
-            s.xk->data[5] ,
-            s.xk->data[6] ,
-            s.xk->data[7] ,
-            s.xk->data[8] ,
-            s.xk->data[9] ,
-            s.xk->data[10],
-            s.xk->data[11],
-            s.xk->data[12],
-            s.xk->data[13],
-            s.xk->data[14],
-            s.xk->data[15],
-            s.xk->data[16]);
-        outfile << epoch << " ";
-        for(int i = 0 ; i < 17 ; ++i)
-            outfile << xi->data[i] << " " ;
-        for(int i = 0 ; i < 7 ; ++i)
-            outfile << yi->data[i] << " " ;
-        for(int i = 0 ; i < 17 ; ++i)
-            outfile << s.xk->data[i] << " " ;
-        outfile << std::endl;
-        outfile_rms << rms << std::endl;
-    }
-    gettimeofday(&after, NULL);
-    double sbefore = before.tv_sec + before.tv_usec * 1E-6;
-    double safter = after.tv_sec + after.tv_usec * 1E-6;
-    double total = safter - sbefore;
-    outfile.close();
-    outfile_rms.close();
-    std::cout << " Run on " << epoch << " epochs ;" << std::scientific << total / double(epoch) << " s./step " << std::endl;
-    printf("I found the following parameters : %e %e %e ; The true parameters being : %e %e %e \n", s.xk->data[0], s.xk->data[1], s.xk->data[6], N0, E0, PSI);
-    std::cout << " Outputs are saved in example-009*.data " << std::endl;
-    std::cout << " You can plot them using e.g. gnuplot : " << std::endl;
-    //std::cout << " gnuplot Data/plot-example-009.gplot ; gv Output/example-009-rms.ps ; gv Output/example-009-Lorentz.ps " << std::endl;
-    /***********************************************/
-    /**** Free the memory ****/
-    /***********************************************/
-    ekf_free(p,s);
+
+// Constructor
+KalmanFilter::KalmanFilter(void) {
 }
