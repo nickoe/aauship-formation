f958c84ff17282b7a32fb144a9e279a5cd212747
Jeppe Dam <jdam09@student.aau.dk>
2014-08-29 21:51:24 +0200

    puha det virker ikke med ctrl node'

--- code/hli/ros/src/aauship/scripts/control-node.py
+++ code/hli/ros/src/aauship/scripts/control-node.py
@@ -3,37 +3,153 @@
 import roslib; roslib.load_manifest('aauship')
 
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import String, Float64MultiArray, Header
+from geometry_msgs.msg import Point, Quaternion, PoseStamped, Pose
 from aauship.msg import *
-
+from nav_msgs.msg import Path
+import scipy.io as sio
+import scipy.linalg as linalg
+import numpy as np
 import time
 import os 
 
 ## This is the control node
 class Control(object):
+    def __init__(self):
+        self.sub = rospy.Subscriber('kf_states', Float64MultiArray, self.callback, queue_size=1)
+        self.pub = rospy.Publisher('lli_input', Float64MultiArray, queue_size=3)
+        rospy.init_node('control_node')
+        self.r = rospy.Rate(0.5) # Hz
+        self.n = 1 # used for wp gen logic
+        self.k = 0
+        # Initilaze parapeters for the PID contorller
+        self.error = []
+        self.integral = []
+        self.integral.append(0)
+        self.derivative = []
+        self.derivative.append(0)
+        self.thrustdiff =[]
+        self.thrustdiff.append(0)
+        self.Kp = 5.0;
+        self.Ki = 0.051;
+        self.Kd = 50.0;
+        self.pubpath = rospy.Publisher('path', Path, queue_size=3)
+        # Create the struct to be printed
+        self.pathmsg = Path()
+        # Assign name
+        self.pathmsg.header.frame_id = "ned"
+        # Load the lawnmower generated path
+        self.path = sio.loadmat('../../../../../matlab/2mmargintrack.mat')
+        h = Header()
+        q = Quaternion(0,0,0,1)
+        for i in self.path['track']:
+            p = Point(i[0],i[1],0)
+            self.pathmsg.poses.append(PoseStamped(h, Pose(p, q)))
+        self.pubpath.publish(self.pathmsg)
+
+
+
     def callback(self, data):
         # send data to lli_input topic
 #        rospy.loginfo(rospy.get_caller_id()+"I heard %s",data.data)
 #        self.ctllog.write(data.data)
         # Publish data to lli_input
         print "control callback" + str(time.time())
-        print os.getcwd()
-        print os.environ
-        pass
+
+        # First time we get a state estimate
+        if self.k == 0:
+            # Include initial point to path once
+            print('Hej')
+            print(self.k)
+            print(data.data[0])
+            print(data.data[1])
+            print(self.path['track'][self.n-1])
+            print(self.path['track'][self.n])
+            self.path['track'] = np.append([[data.data[0],data.data[1]]], self.path['track'], axis=0)
+
+        # GNC
+        (headingdesired, wp_reached, cte) = self.wp_gen(self.path['track'][self.n-1],self.path['track'][self.n],np.array([data.data[0],data.data[1]])); # WP Gen
+        if (wp_reached == 1):
+            self.n = self.n+1;
+            if self.n >= len(self.path['track']):
+                #es = k;
+                print('Trying to break')
+                #break
+
+        # PID
+        self.error.append(self.rad2pipi(headingdesired  - x_hat[6]))
+        self.integral.append(integral[self.k] + error[self.k])
+        if self.k!=1:
+            self.derivative.append(self.error[self.k] - self.error[k-1])
+        self.thrustdiff.append(Kp*self.error[self.k] + Ki*self.integral[self.k] + Kd*self.derivative[self.k])
+        print("error " + str(self.error[self.k]))
+        print("integral " + str(self.integral[self.k]))
+        print("derivative " + str(self.derivative[self.k]))
+        print("thrustdiff " + str(self.thrustdiff[self.k]))
+        self.k = self.k+1
+
+
+    # This calculates reference points for the path follower
+    def wp_gen(self, wps, wpe, now):
+        
+        #P_c = [now, 0]; # [x y angle]
+        P_c = now; # [x y]
+        wp_r = 1; # Waypoint Radius
+        wp_reached = 0; # Waypoint not reached
+        v_i_len = 2; # length of intermediate vector
+        
+        ## Initial calculations
+        #track = [wps;wpe];
+
+        ## Track-frame projected point
+        v_i = ( (wpe-wps)/linalg.norm(wpe-wps) )*v_i_len; # Intermediate vector
+
+        #P_i = [P_c[0]+v_i[0], P_c[1]+v_i[1]]; # Intermediate projected parallel point
+        P_i = P_c+v_i
+
+        ## Calculate projected point onto the track
+        A = P_i - wps;
+        B = wpe - wps;
+        r_p = ((A.dot(B))/((linalg.norm(B)**2)))*B; # Projection of vector on vector
+        P_p = r_p + wps;
+
+        ## The vessels predicted position
+        v_d = P_p - P_c[0:2];
+        v_ref = v_d/linalg.norm(v_d); # normaliserer
+        v_ref = v_ref * v_i_len;
+        P_ref = v_ref + now;
+        # plot(P_ref(2),P_ref(1),'*')
+
+        ## Calculate if waypoint is reached
+        dist = sqrt((P_c[0]-wpe[0])**2+(P_c[1]-wpe[1])**2);
+        if dist < wp_r:
+            wp_reached = 1
+
+        ## Calculate heading
+        # heading = 2*pi-asin(( P_ref(1)-P_c[0] ) / sqrt( (P_ref(1)-P_c[0])  ^2 + (P_ref(2)-P_c[1])^2 ));
+        # heading = mod(atan2(v_ref(2),v_ref(1))+pi,2*pi)-pi;
+        heading = self.rad2pipi(atan2(v_ref[1],v_ref[0]))
+
+        ## CrossTrackError % MOD = HYP*SIN(A) => crosstrack = (pos-wps) * sin(vinkel mellem (pos-wps) og (pos-wps))
+        a = linalg.norm(now-wps);
+        b = linalg.norm(wpe-wps);
+        vinkel = (acos((now-wps).dot((wpe-wps).T)/(a*b)));
+        # vinkeligrader = vinkel*180/pi;
+        cte = a*sin(vinkel);
+
+        #return {'heading':heading, 'wp_reached':wp_reached, 'cte':cte}
+        return (heading, wp_reached, cte)
 
     def run(self):
-	BUFSIZE = 1024
+        BUFSIZE = 1024
         #self.ctllog = open(os.getcwd() + "/../meas/ctl.log",'w')
         ##self.ctllog = open("logs/ctl.log",'w',BUFSIZE)
         ##print(self.ctllog.name)
-        sub = rospy.Subscriber('control_input', Faps, self.callback)
-        pub = rospy.Publisher('lli_input', Faps)
-        rospy.init_node('control_node')
-        r = rospy.Rate(0.5) # Hz
+
         #rospy.spin() # Keeps the node running untill stopped
         while not rospy.is_shutdown():
             #pub.publish("control signals should be sent here")
-            r.sleep()
+            self.r.sleep()
         print("\nClosing log file")
         ##self.ctllog.close()
         print("Exiting")
--- code/hli/ros/src/aauship/scripts/simulation-node.py
+++ code/hli/ros/src/aauship/scripts/simulation-node.py
@@ -22,9 +22,8 @@ import os
 ## This is the simulaiton node, basically kind of the same as simaauship.m
 class Simulator(object):
     def __init__(self):
-        #self.sub = rospy.Subscriber('lli_input', Faps, self.callback)
-        self.pub = rospy.Publisher('kf_states', Float64MultiArray, queue_size=3)
-        self.pubpath = rospy.Publisher('path', Path, queue_size=3)
+        self.sub = rospy.Subscriber('lli_input', Float64MultiArray, self.callback)
+        self.pub = rospy.Publisher('kf_states', Float64MultiArray, queue_size=1)
         self.trackpath = rospy.Publisher('track', Path, queue_size=3)
         self.refpath = rospy.Publisher('refpath', Path, queue_size=3)
         self.keepoutpath = rospy.Publisher('keepout', Path, queue_size=3)
@@ -38,27 +37,22 @@ class Simulator(object):
         self.R = np.diag(self.v)
         self.R_i = np.diag(self.v)
 
+        self.thrustdiff = 0
         self.u = np.zeros(5) # input vector
         self.x = np.zeros(17) # state vector
         self.pubmsg = Float64MultiArray()
-        self.pathmsg = Path()
-        self.pathmsg.header.frame_id = "ned"
         self.refmsg = Path()
         self.refmsg.header.frame_id = "ned"
         self.keepoutmsg = Path()
         self.keepoutmsg.header.frame_id = "ned"
-        self.path = sio.loadmat('../../../../../matlab/2mmargintrack.mat')
         self.klingen = sio.loadmat('klingenberg.mat')
-
+        self.path = sio.loadmat('../../../../../matlab/2mmargintrack.mat')
         self.trackmsg = Path()
         self.trackmsg.header.frame_id = "ned"
 
         h = Header()
         q = Quaternion(0,0,0,1)
-        for i in self.path['track']:
-            p = Point(i[0],i[1],0)
-            self.pathmsg.poses.append(PoseStamped(h, Pose(p, q)))
-	
+
         # klingen['inner'][0]  is northing
         # klingen['inner'][1]  is easting
         for i in self.klingen['outer']:
@@ -70,12 +64,9 @@ class Simulator(object):
             self.keepoutmsg.poses.append(PoseStamped(h, Pose(p, q)))
         
     def callback(self, data):
-        # Set the input according to the control signal.
-        print "lli input callback" + str(time.time())
-        print os.getcwd()
-        print os.environ
-        self.u = np.array([8,0,0,0,0])
-        pass
+        print(data.data[0])
+        self.thrustdiff = data.data[0]      
+        
 
     # Angle in rad to the interval (-pi pi]
     def rad2pipi(self, rad):
@@ -93,57 +84,6 @@ class Simulator(object):
         y = r - s*pi;
         '''
 
-    # This calculates reference points for the path follower
-    def wp_gen(self, wps, wpe, now):
-        
-        #P_c = [now, 0]; # [x y angle]
-        P_c = now; # [x y]
-        wp_r = 1; # Waypoint Radius
-        wp_reached = 0; # Waypoint not reached
-        v_i_len = 2; # length of intermediate vector
-        
-        ## Initial calculations
-        #track = [wps;wpe];
-
-        ## Track-frame projected point
-        v_i = ( (wpe-wps)/linalg.norm(wpe-wps) )*v_i_len; # Intermediate vector
-
-        #P_i = [P_c[0]+v_i[0], P_c[1]+v_i[1]]; # Intermediate projected parallel point
-        P_i = P_c+v_i
-
-        ## Calculate projected point onto the track
-        A = P_i - wps;
-        B = wpe - wps;
-        r_p = ((A.dot(B))/((linalg.norm(B)**2)))*B; # Projection of vector on vector
-        P_p = r_p + wps;
-
-        ## The vessels predicted position
-        v_d = P_p - P_c[0:2];
-        v_ref = v_d/linalg.norm(v_d); # normaliserer
-        v_ref = v_ref * v_i_len;
-        P_ref = v_ref + now;
-        # plot(P_ref(2),P_ref(1),'*')
-
-        ## Calculate if waypoint is reached
-        dist = sqrt((P_c[0]-wpe[0])**2+(P_c[1]-wpe[1])**2);
-        if dist < wp_r:
-            wp_reached = 1
-
-        ## Calculate heading
-        # heading = 2*pi-asin(( P_ref(1)-P_c[0] ) / sqrt( (P_ref(1)-P_c[0])  ^2 + (P_ref(2)-P_c[1])^2 ));
-        # heading = mod(atan2(v_ref(2),v_ref(1))+pi,2*pi)-pi;
-        heading = self.rad2pipi(atan2(v_ref[1],v_ref[0]))
-
-        ## CrossTrackError % MOD = HYP*SIN(A) => crosstrack = (pos-wps) * sin(vinkel mellem (pos-wps) og (pos-wps))
-        a = linalg.norm(now-wps);
-        b = linalg.norm(wpe-wps);
-        vinkel = (acos((now-wps).dot((wpe-wps).T)/(a*b)));
-        # vinkeligrader = vinkel*180/pi;
-        cte = a*sin(vinkel);
-
-        #return {'heading':heading, 'wp_reached':wp_reached, 'cte':cte}
-        return (heading, wp_reached, cte)
-
     def run(self):
         BUFSIZE = 1024
         #self.ctllog = open(os.getcwd() + "/../meas/ctl.log",'w')
@@ -152,23 +92,10 @@ class Simulator(object):
 
         f = kfoo.KF()
         time.sleep(3)
-        self.pubpath.publish(self.pathmsg)
         self.refpath.publish(self.refmsg)
         self.keepoutpath.publish(self.keepoutmsg)
 
-        # Initilaze parapeters for the PID contorller
-        error = []
-        integral = []
-        integral.append(0)
-        derivative = []
-        derivative.append(0)
-        thrustdiff =[]
-        thrustdiff.append(0)
-        Kp = 5.0;
-        Ki = 0.051;
-        Kd = 50.0;
-        
-	    # Testing output of rad2pipi funciton
+        # Testing output of rad2pipi funciton
         '''
         a = np.array([-12.5664,-12.0664,-11.5664,-11.0664,-10.5664,-10.0664, -9.5664, -9.0664, -8.5664, -8.0664, -7.5664, -7.0664, -6.5664, -6.0664, -5.5664, -5.0664, -4.5664, -4.0664, -3.5664, -3.0664, -2.5664, -2.0664, -1.5664, -1.0664, -0.5664, -0.0664,  0.4336,  0.9336,  1.4336,  1.9336,  2.4336,  2.9336,  3.4336,  3.9336,  4.4336,  4.9336,  5.4336,  5.9336,  6.4336,  6.9336,  7.4336,  7.9336,  8.4336,  8.9336,  9.4336,  9.9336, 10.4336, 10.9336, 11.4336, 11.9336, 12.4336])
         for x in a:
@@ -193,7 +120,7 @@ class Simulator(object):
 
 
         k = 0
-        n = 1 # used for wp gen logic
+        
 
         # Initialize an poses array for the trackmsg
         h = Header()
@@ -217,16 +144,8 @@ class Simulator(object):
             q = Quaternion(0,0,0,1)
             self.trackmsg.poses[0] = PoseStamped(h, Pose(p, q))
 
-            # GNC
-            (headingdesired, wp_reached, cte) = self.wp_gen(self.path['track'][n-1],self.path['track'][n],np.array([self.x[0],self.x[1]])); # WP Gen
-            if (wp_reached == 1):
-                n = n+1;
-                if n >= len(self.path['track']):
-                    es = k;
-                    print('Trying to break')
-                    break
-
-            self.u = np.array([8,0,0,0,thrustdiff[k]])
+            # Her skal u subscripe til lli input
+            self.u = np.array([8,0,0,0,self.thrustdiff])
 
             # Simulation
             self.x = f.aaushipsimmodel(self.x,self.u)
@@ -239,16 +158,16 @@ class Simulator(object):
             self.z[5:7] = self.x[12:14] + np.array([self.v[5],self.v[6]])*np.random.randn(1,2)
 
             if k%20 != 0:
-                print("stor R")
+                #print("stor R")
                 self.R[0,0] = 10*10**10;
                 self.R[1,1] = 10*10**10; 
-                print(self.R)
+                #print(self.R)
 
             else:
-                print("orig R")
+                #print("orig R")
                 self.R[0,0] = self.R_i[0,0]
                 self.R[1,1] = self.R_i[1,1]
-                print(self.R)
+                #print(self.R)
                 #gpsc[jj] = k;
                 jj = jj+1;
             
@@ -256,7 +175,7 @@ class Simulator(object):
             
             for a in x_hat:
                 self.pubmsg.data.append(a)
-                print(a)
+                #print(a)
             self.pub.publish(self.pubmsg)
 
             # Send tf for the robot model visualisation
@@ -276,18 +195,6 @@ class Simulator(object):
             self.trackpath.publish(self.trackmsg)
             #print(self.pathmsg)
 
-
-            # PID
-            error.append(self.rad2pipi(headingdesired  - x_hat[6]))
-            integral.append(integral[k] + error[k])
-            if k!=1:
-                derivative.append(error[k] - error[k-1])
-            thrustdiff.append(Kp*error[k] + Ki*integral[k] + Kd*derivative[k])
-            
-            print("error " + str(error[k]))
-            print("integral " + str(integral[k]))
-            print("derivative " + str(derivative[k]))
-            print("thrustdiff " + str(thrustdiff[k]))
             #rospy.signal_shutdown("testing")
             k = k+1
             print(time.time())
