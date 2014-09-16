4fbf50b4d67f61a657acadc31d2d6f765246d9f2
Nick Østergaard <oe.nick@gmail.com>
2014-09-16 10:38:19 +0200

    Add rotation to the result

--- code/hli/ros/src/aauship/src/ahrs-mahony-node.cpp
+++ code/hli/ros/src/aauship/src/ahrs-mahony-node.cpp
@@ -41,19 +41,59 @@ public:
     //u.calculateEulerAngles();
     //ROS_INFO("Euler angles: [%.3f, %.3f, %.3f]", u.getEulerAngles(0), u.getEulerAngles(1), u.getEulerAngles(2));
 
+    // It seems like the filter computes the attitude in ENU not in NED, so we rotate.
+    tf::Quaternion v(0,0,0,1);
+    v = tf::createQuaternionFromRPY(3.1514, 0, 0);
+    
+    /* Example of doing quaternion rotaion manually
+    tf::Quaternion Qorien = tf::Quaternion(0.0, 0.0, 0.0, 1.0);
+    double angleX, angleY, angleZ;
+    angleX = 3.1415;
+    angleY = 0.0;
+    angleZ = 0;
+
+    double cosX,sinX,cosY,sinY,cosZ,sinZ;
+    cosX = cos(angleX/2); sinX = sin(angleX/2);
+    cosY = cos(angleY/2); sinY = sin(angleY/2);
+    cosZ = cos(angleZ/2); sinZ = sin(angleZ/2);
+    tf::Quaternion QrotX,QrotY,QrotZ;
+
+    QrotX = tf::Quaternion( 1.0*sinX,
+                            0.0*sinX,
+                            0.0*sinX,
+                            cosX);
+    QrotY = tf::Quaternion( 0.0*sinY,
+                            1.0*sinY,
+                            0.0*sinY,
+                            cosY);
+    QrotZ = tf::Quaternion( 0.0*sinZ,
+                            0.0*sinZ,
+                            1.0*sinZ,
+                            cosZ);
+    tf::Quaternion Qtotal = QrotX*QrotY*QrotZ;
+    Qorien = Qtotal * q; // The new quaternion
+    */
+
     /* Publish rviz transform information */
     static tf::TransformBroadcaster tfbc;
     tf::Transform transform;
     transform.setOrigin( tf::Vector3(0,0,0) );
-    transform.setRotation(q);
-    //tfbc.sendTransform( tf::StampedTransform(transform, ros::Time::now(), "map", "boat_link"));
+    //transform.setRotation(Qorien);
+
+    tf::Quaternion nedq;
+    nedq = v*q;
+    transform.setRotation(nedq);
+    //tfbc.sendTransform( tf::StampedTransform(transform, ros::Time::now(), "ned", "boat_link"));
 
     // Publish attitude information (for use with i.e. the Kalman filter)
     geometry_msgs::Quaternion msgq;
+    /*
     msgq.x = u.getQuaternions(1);
     msgq.y = u.getQuaternions(2);
     msgq.z = u.getQuaternions(3);
     msgq.w = u.getQuaternions(0);
+    */
+    tf::quaternionTFToMsg(nedq, msgq);
     attitudepub.publish(msgq);
 
   }
