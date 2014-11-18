0497984ed0f5b6288ccebfb099e1ae9368548d7e
Nick Østergaard <oe.nick@gmail.com>
2014-11-18 22:28:39 +0100

    MB100 node that should work, not tested on hardware

--- code/hli/ros/src/aauship/scripts/mb100-node.py
+++ code/hli/ros/src/aauship/scripts/mb100-node.py
@@ -18,128 +18,61 @@ import gpsfunctions
 
 class MB100(object):
     def __init__(self):
-        pass
+        BUFSIZE = 1024
+        self.mb100log = open("logs/mb100.log",'wb',BUFSIZE)
+        self.mb100rcv = serial.Serial("/dev/ttyUSB0",115200,timeout=0.04)
+
+    def parse(self,line):
+        self.mb100log.write(line + ',' + str(time.time()) + "\r\n")
+        line = line.split(',')
+        if line[0] == "$PASHR" and line[1] == "POS":
+            mb100 = {}
+            mb100['posmode'] = int(line[2])
+            mb100['sats'] = int(line[3])
+            mb100['utctime'] = float(line[4])
+            [latdec, londec] = (gpsfunctions.nmea2decimal(float(line[5]),line[6],float(line[7]),line[8]))
+            mb100['lat'] = latdec*pi/180.0
+            mb100['lon'] = londec*pi/180.0
+            mb100['alt'] = float(line[9])
+            mb100['ageofdiff'] = float(line[10])
+            mb100['cog'] = float(line[11])*pi/180.0
+            mb100['sog'] = float(line[12])*0.514444444
+            mb100['verticalvel'] = float(line[13])
+            mb100['pdop'] = float(line[14])
+            mb100['hdop'] = float(line[15])
+            mb100['vdop'] = float(line[16])
+            mb100['tdop'] = float(line[17])
+            mb100['crc'] = line[18]
+            return mb100
+        else:
+            return None
 
     def run(self):
-        #self.qu = Queue.Queue()
-        #self.packet = fapsPacket.Handler('/dev/ttyUSB0', 115200, 0.02, self.qu)
+        # Example of data
+        #line = "$PASHR,POS,1,10,134035.35,5700.8745400,N,00959.1551112,E,059.318,10.4,077.3,000.459,+000.069,1.7,0.9,1.4,0.9,Hp23*30"
+        #print(self.parse(line))
 
-        BUFSIZE = 1024
-        #mb100log = open("logs/gps2.log",'wb',BUFSIZE)
-
-        '''
-        try:
-            gps2 = gps2rcv.readline()
-            if gps2 != "":
-                gps2 = gps2.rstrip()
-                gps2log.write(gps2 + ',' +str(time.time()) + "\r\n")
-        except Exception:
-            pass
-        '''
-
-        line = "$PASHR,POS,1,10,134035.35,5700.8745400,N,00959.1551112,E,059.318,10.4,077.3,000.459,+000.069,1.7,0.9,1.4,0.9,Hp23*30"
-        line = line.split(',')
-        posmod = float(line[2])
-        sats = float(line[3])
-        utctime = float(line[4])
-        [latdec, londec] = (gpsfunctions.nmea2decimal(float(line[5]),line[6],float(line[7]),line[8]))
-        lat = latdec*pi/180
-        lon = londec*pi/180
-        alt = float(line[9])
-        ageofdiff = float(line[10])
-        cog = float(line[11])*pi/180
-        sog = float(line[12])*0.514444444
-        verticalvel = float(line[13])
-        pdop = float(line[14])
-        hdop = float(line[15])
-        vdop = float(line[16])
-        tdop = float(line[17])
-        crc = line[18]
-        
-        print(lat,lon)
-        '''
-        [latdec, londec] = (gpsfunctions.nmea2decimal(float(line[2]),line[3],float(line[4]),line[5]))
-        latdec = latdec*pi/180
-        londec = londec*pi/180
-        if self.centerlat == 0 and self.centerlon == 0:
-            self.rot=gpsfunctions.get_rot_matrix(float(latdec),float(londec))
-            self.centerlat = float(latdec)
-            self.centerlon = float(londec)
-        pos = self.rot * (gpsfunctions.wgs842ecef(float(latdec),float(londec))-gpsfunctions.wgs842ecef(float(self.centerlat),float(self.centerlon)))
-
-
-
-        line = "".join(packet['Data']).split(',')
-        if line[0] == "$GPRMC" and line[2] == 'A':
-        
-            #print ",".join("".join(packet['Data']).split(',')[1:8])
-            line = line[1:8]
-
-
-        if line[1] == 'A' :
-            self.gpsinvalid += 1
-        
-        if 42 <= self.gpsinvalid <= 67:
-            line[1] = 'V'
-            print "Gps invalid!"
-        
-        if line[1] == 'A' :
-        
-            self.gpslog.write(",".join(line) + ", " + str(time.time()) + "\r\n")
-            self.fulllog.write(",".join(line) + ", " + str(time.time()) + "\r\n")
-            #print line
-            speed = float(line[6]) * 0.514444444 #* 0 + 100
-            #print str(speed) + " m/s"
-            [latdec, londec] = (gpsfunctions.nmea2decimal(float(line[2]),line[3],float(line[4]),line[5]))
-            latdec = latdec*pi/180
-            londec = londec*pi/180
-            if self.centerlat == 0 and self.centerlon == 0:
-                self.rot=gpsfunctions.get_rot_matrix(float(latdec),float(londec))
-                self.centerlat = float(latdec)
-                self.centerlon = float(londec)
-            
-            
-            pos = self.rot * (gpsfunctions.wgs842ecef(float(latdec),float(londec))-gpsfunctions.wgs842ecef(float(self.centerlat),float(self.centerlon)))
-            #print pos
-            #print pos
-            
-            
-            self.state[0] = [float(pos[0,0]),        1]
-            #self.state[0] = [10,1]
-            self.state[1] = [speed, 1]
-            self.state[3] = [float(pos[1,0]),     1]
-            #self.state[3] = [5,1]
-        '''
-
-
-
-
-        
-        '''
-        time.sleep(5)
-        #self.packet.start()
-        pub = rospy.Publisher('', Faps, queue_size=10)
+        pub = rospy.Publisher('kf_states', Float64MultiArray, queue_size=1)
         rospy.init_node('mb100')
-        r = rospy.Rate(100) # Rate in Hz, maybe try a faster rate than
-        # 100 to fix the buffering issue
-        '''
+        r = rospy.Rate(100)
 
-        '''
         while not rospy.is_shutdown():
-            # Grabbing the GPS2 data (tempory implementation)
+            # Grabbing the data
             try:
-                gps2 = gps2rcv.readline()
-                if gps2 != "":
-                    gps2 = gps2.rstrip()
-                    gps2log.write(gps2 + ',' +str(time.time()) + "\r\n")
+                data = selfmb100rcv.readline()
+                if data != "":
+                    data = data.rstrip()
+                    self.parse(data)
             except Exception:
                 pass
 
             # End of loop, wait to keep the rate
             r.sleep()
-        '''
+        
+        print("Closing MB100 node")
+        self.mb100log.close()
+        self.mb100rcv.close()
 
-        #mb100log.close()
         
 if __name__ == '__main__':
     w = MB100()
