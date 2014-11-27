01b3f5be228a569ced1fea4669e7a5da44919eda
Jeppe Dam <jdam09@student.aau.dk>
2014-11-27 16:16:10 +0100

    pew pew moar plots

--- code/matlab/Fcatest.m
+++ code/matlab/Fcatest.m
@@ -2,38 +2,50 @@ clear all;
 
 %% 3D plot with force magnitude
 
-step = 0.1;
-[X,Y] = meshgrid(-20:step:20,-20:step:20);
-Ftot = zeros(length(X), length(Y),2);
+step = 0.3;
+[X,Y] = meshgrid(-30:step:20,-40:step:40);
+X=X';
+Y=Y';
+lenx=length(X(:,1));
+leny=length(Y(1,:));
+Ftot = zeros(lenx, leny,2);
 rsav = 5;
 vl = [10,10];
-p0 = [2,2];
+pi0 = [2,2];
 Kvl = 2;
 Kij = 0.1;
 Kca = 20;
 Koa = 20;
 
-for m = 1:length(X);
-    for n = 1:length(Y);
-        p = [X(1,m),Y(n,1)];
-        Fvl = Kvl*(vl-p-(vl-p0));
+Fijmagn = zeros(lenx,leny);
+
+for m = 1:lenx;
+    for n = 1:leny;
+        pi = [X(m,1),Y(1,n)];
+        Fvl = Kvl*(vl-pi-(vl-pi0));
         Fvlmagn(m,n) = norm(Fvl);
-        
-        pj = [4 , 4];
-        dist = p - pj;
-        d0ij = p0 - pj;
-        Fij = Kij*(dist-d0ij);
-        Fijmagn(m,n) = norm(Fij);
-        
-        if norm(dist) < rsav
-            Fca = ((Kca*rsav)/norm(dist)-Kca)*(dist/norm(dist));
+
+        pj(1,1:2) = [10 , 10];
+        pj(2,1:2) = [15 , 0];
+        pj0(1,1:2) = [-5 , -1];
+        pj0(2,1:2) = [15 , 0];
+        for j = 1:1
+            Fij(j,1:2) = Kij*(pj(j,1:2)-pi-(pj0(j,1:2)-pi0));
+            dij(j,1:2) = pj(j,1:2) - pi;%0,-1
+%             d0ij(j,1:2) = pj0(j,1:2) - pi0;%-7,-3
+%             Fij(j,1:2) = Kij*(dij(j,1:2)-(d0ij(j,1:2)));%7,3
+            Fijmagn(m,n) = norm(Fij(j,1:2));
+        end
+                
+        if norm(dij) < rsav
+            Fca = ((Kca*rsav)/norm(dij)-Kca)*(dist/norm(dij));
         else
             Fca = 0;
         end
         Fcamagn(m,n) = norm(Fca);
         
         po1 = [-6,-6];
-        dist = p - po1;
+        dist = pi - po1;
         if norm(dist) < rsav
             Foa = (Koa/norm(dist)-Koa/rsav)*(dist/norm(dist));
         else
@@ -57,21 +69,22 @@ hold on
 axis equal
 density = 10;
 [xvel,yvel] = gradient(-Fvlmagn(1:density:m,1:density:n),step,step);
-contour(X, Y, Fvlmagn);
-quiver(X(1:density:m,1:density:n), Y(1:density:m,1:density:n),xvel,yvel);
+% contour(X, Y, Fvlmagn);
+% quiver(X(1:density:m,1:density:n), Y(1:density:m,1:density:n),xvel,yvel);
 title('Contour and quiver plot of Fvl')
 hold off
 figure(2)
 clf;
 hold on
 surf(X, Y, Fvlmagn);
-contour(X, Y, Fvlmagn);
+% contour(X, Y, Fvlmagn);
 title('Fvlmagn')
 hold off
 figure(3)
 clf;
 hold on
-surf(X, Y, Fijmagn);
+surf(X,Y, Fijmagn);
+axis equal
 title('Fijmagn')
 hold off
 figure(4)
@@ -91,24 +104,27 @@ clf;
 hold on
 density = 4;
 [totxvel,totyvel] = gradient(-Ftotmagn(1:density:m,1:density:n),step,step);
-contour(X, Y, Ftotmagn);
-quiver(X(1:density:m,1:density:n), Y(1:density:m,1:density:n),totxvel,totyvel);
+% contour(X, Y, Ftotmagn);
+% quiver(X(1:density:m,1:density:n), Y(1:density:m,1:density:n),totxvel,totyvel);
 clear m, clear n
-m(1) = 40;
-n(1) = 40;
-for k = 1:1000;
-    A = Ftotmagn(m(k)-1:m(k)+1,n(k)-1:n(k)+1);
-    [minval,index] = min(A(:));
-    [i,j] = ind2sub(size(A),index);
-    m(k+1) = m(k)+(i-2);
-    n(k+1) = n(k)+(j-2);
-    xny(k) = X(m(k),n(k));
-    yny(k) = Y(m(k),n(k));
-end
-plot(xny,yny)
-plot(X(m(1),n(1)),Y(m(1),n(1)),'r*')
+% m(1) = 40;
+% n(1) = 40;
+% for k = 1:1000;
+%     A = Ftotmagn(m(k)-1:m(k)+1,n(k)-1:n(k)+1);
+%     [minval,index] = min(A(:));
+%     [i,j] = ind2sub(size(A),index);
+%     m(k+1) = m(k)+(i-2);
+%     n(k+1) = n(k)+(j-2);
+%     xny(k) = X(m(k),n(k));
+%     yny(k) = Y(m(k),n(k));
+% end
+% plot(xny,yny,'r-*')
+% plot(X(m(1),n(1)),Y(m(1),n(1)),'r*')
 surf(X,Y,Ftotmagn-100);
 axis equal
+xlabel('x')
+ylabel('y')
+zlabel('z')
 hold off
 
 
